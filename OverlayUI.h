b5c68d7ba7207a4adf7f1feeac99c122ddfb7b90
AgentDudu <rafaeldumatubun23@gmail.com>
2026-08-21 19:12:20 +0700

    Feat: Change mouse drawing design to mimic real life shape

--- OverlayUI.h
+++ OverlayUI.h
@@ -29,6 +29,65 @@ private:
         DeleteObject(hFont);
     }
 
+    static void DrawMouseUI(HDC hdc, int x, int y) {
+        AppState& state = AppState::Get();
+        int sx = (int)(x * state.uiScale);
+        int sy = (int)(y * state.uiScale);
+        
+        int bw = (int)(35 * state.uiScale);
+        int bh = (int)(45 * state.uiScale);
+        int gap = (int)(5 * state.uiScale);
+        int bodyH = (int)(55 * state.uiScale);
+        int corner = (int)(15 * state.uiScale);
+
+        HBRUSH lmbBrush = CreateSolidBrush(state.lmb ? state.activeBg : state.inactiveBg);
+        HBRUSH rmbBrush = CreateSolidBrush(state.rmb ? state.activeBg : state.inactiveBg);
+        HBRUSH bodyBrush = CreateSolidBrush(state.inactiveBg);
+        HBRUSH wheelBrush = CreateSolidBrush(state.inactiveText); // Scroll wheel matches text color
+
+        HPEN nullPen = CreatePen(PS_NULL, 0, RGB(0,0,0));
+        HPEN oldPen = (HPEN)SelectObject(hdc, nullPen);
+
+        SelectObject(hdc, lmbBrush);
+        RoundRect(hdc, sx, sy, sx + bw, sy + bh, corner, corner);
+        
+        SelectObject(hdc, rmbBrush);
+        RoundRect(hdc, sx + bw + gap, sy, sx + bw * 2 + gap, sy + bh, corner, corner);
+        
+        SelectObject(hdc, bodyBrush);
+        RoundRect(hdc, sx, sy + bh + (int)(3 * state.uiScale), sx + bw * 2 + gap, sy + bh + (int)(3 * state.uiScale) + bodyH, corner * 2, corner * 2);
+
+        SelectObject(hdc, wheelBrush);
+        int wx = sx + bw - (int)(1 * state.uiScale);
+        int wy = sy + (int)(15 * state.uiScale);
+        int ww = gap + (int)(2 * state.uiScale);
+        int wh = (int)(18 * state.uiScale);
+        RoundRect(hdc, wx, wy, wx + ww, wy + wh, (int)(4 * state.uiScale), (int)(4 * state.uiScale));
+
+        SetBkMode(hdc, TRANSPARENT);
+        HFONT hFont = CreateFontW((int)(16 * state.uiScale), 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE, 
+                                  DEFAULT_CHARSET, OUT_OUTLINE_PRECIS, CLIP_DEFAULT_PRECIS, 
+                                  CLEARTYPE_QUALITY, VARIABLE_PITCH, L"Segoe UI");
+        HFONT oldFont = (HFONT)SelectObject(hdc, hFont);
+
+        RECT rcLmb = { sx, sy, sx + bw, sy + bh };
+        SetTextColor(hdc, state.lmb ? state.activeText : state.inactiveText);
+        DrawTextW(hdc, L"L", -1, &rcLmb, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+
+        RECT rcRmb = { sx + bw + gap, sy, sx + bw * 2 + gap, sy + bh };
+        SetTextColor(hdc, state.rmb ? state.activeText : state.inactiveText);
+        DrawTextW(hdc, L"R", -1, &rcRmb, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+
+        SelectObject(hdc, oldFont);
+        SelectObject(hdc, oldPen);
+        DeleteObject(nullPen);
+        DeleteObject(hFont);
+        DeleteObject(lmbBrush);
+        DeleteObject(rmbBrush);
+        DeleteObject(bodyBrush);
+        DeleteObject(wheelBrush);
+    }
+
     static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
         AppState& state = AppState::Get();
 
@@ -54,8 +113,8 @@ private:
             DrawButton(memDC, 15, 110, 145, 25, L"SPACE", state.keys[VK_SPACE]);
             DrawButton(memDC, 15, 140, 70, 25, L"SHIFT", state.keys[VK_LSHIFT] || state.keys[VK_SHIFT]);
             DrawButton(memDC, 90, 140, 70, 25, L"CTRL", state.keys[VK_LCONTROL] || state.keys[VK_CONTROL]);
-            DrawButton(memDC, 170, 10, 35, 70, L"LMB", state.lmb);
-            DrawButton(memDC, 210, 10, 35, 70, L"RMB", state.rmb);
+            
+            DrawMouseUI(memDC, 170, 10);
 
             BitBlt(hdc, 0, 0, rect.right, rect.bottom, memDC, 0, 0, SRCCOPY);
 
